b8b6df0c865cd70f6f89cf5545c48c5641bc87ea
nvdai2401 <nvdai2401@gmail.com>
2020-06-17 14:03:42 +0700

    develop UI and setup base config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './evdict.svg'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [searchText, setSearchText] = useState('')
+	const [suggestionVisible, setSuggestionVisible] = useState(false)
+
+	const toggleSuggestion = (value) => {
+		setSuggestionVisible(value)
+	}
+
+	return (
+		<div className='root'>
+			<div className='header'>
+				<img src={logo} alt='EVDict logo' className='logo' />
+				<div className='search-box'>
+					<input
+						placeholder='Tra từ điển Anh Việt'
+						type='search'
+						value={searchText}
+						onFocus={() => toggleSuggestion(true)}
+						onBlur={() => toggleSuggestion(false)}
+						onChange={(e) => setSearchText(e.target.value)}
+					/>
+					<button>Search</button>
+					{suggestionVisible && (
+						<ul className='suggestions'>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+							<li>Lorem ipsum</li>
+						</ul>
+					)}
+				</div>
+			</div>
+
+			<div className='main'>
+				<p className='word'>Word</p>
+				<p className='pronunciation'>/ə'gouiɳ/</p>
+				<div className='definition'>
+					<p className='type'>tính từ & phó từ</p>
+					<div>đang chạy, đang chuyển động; đang hoạt động, đang tiến hành</div>
+					<p>to move counters of an abacus</p>
+					<p>to work an abacus+ tính bằng bàn tính, gảy bàn tính</p>
+				</div>
+				<div className='definition'>
+					<p className='type'>tính từ & phó từ</p>
+					<div>đang chạy, đang chuyển động; đang hoạt động, đang tiến hành</div>
+					<p>to move counters of an abacus</p>
+					<p>to work an abacus+ tính bằng bàn tính, gảy bàn tính</p>
+				</div>
+				<div className='definition'>
+					<p className='type'>tính từ & phó từ</p>
+					<div>đang chạy, đang chuyển động; đang hoạt động, đang tiến hành</div>
+					<p>to move counters of an abacus</p>
+					<p>to work an abacus+ tính bằng bàn tính, gảy bàn tính</p>
+				</div>
+				<div className='definition'>
+					<p className='type'>tính từ & phó từ</p>
+					<div>đang chạy, đang chuyển động; đang hoạt động, đang tiến hành</div>
+					<p>to move counters of an abacus</p>
+					<p>to work an abacus+ tính bằng bàn tính, gảy bàn tính</p>
+				</div>
+				<div className='definition'>
+					<p className='type'>tính từ & phó từ</p>
+					<div>đang chạy, đang chuyển động; đang hoạt động, đang tiến hành</div>
+					<p>to move counters of an abacus</p>
+					<p>to work an abacus+ tính bằng bàn tính, gảy bàn tính</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
